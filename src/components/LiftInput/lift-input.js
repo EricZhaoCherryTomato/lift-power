aff167edf6332b66a5b97a685c269d937418e5cb
Eric Zhao <eric.zhao@virginaustralia.com>
2020-08-10 16:47:34 +1000

    valide the front end input

--- src/components/LiftInput/lift-input.js
+++ src/components/LiftInput/lift-input.js
@@ -8,11 +8,25 @@ class LiftInput extends React.Component {
     }
     mySubmitHandler = (event) => {
         event.preventDefault();
-        this.setState({ result: calculateBlock(this.state.command) });
+        const allowedMethod = ['P', 'M', 'L'];
+        let validMethd = true;
+        this.state.command.split('').forEach(m => { if (!allowedMethod.includes(m.toUpperCase()) && validMethd) { alert('Robot only understand P, M, L command, please type in from these three command. Thank you ;)'); validMethd = false; } })
+        if (validMethd) { this.setState({ result: 'The result :' + calculateBlock(this.state.command.toUpperCase()) }); }
+        else {
+            this.setState({ result: 'Please check the command above' });
+        }
+
     }
     myChangeHandler = (event) => {
+        // debugger;
         this.setState({ command: event.target.value });
+
     }
+    // checkCommand = (e) => {
+    //     console.log(e.key)
+
+    //     if (!['P', 'M', 'L', 'p', 'm', 'l'].includes(e.key)) { alert('Robot only understand P, M, L command, please type in from these three command. Thank you ;)'); }
+    // }
     render() {
         return (
             <form onSubmit={this.mySubmitHandler}>
@@ -20,11 +34,12 @@ class LiftInput extends React.Component {
                 <input
                     type='text'
                     onChange={this.myChangeHandler}
+                // onKeyPress={this.checkCommand}
                 />
                 <input
                     type='submit'
                 />
-                <h4> The result :{this.state.result}</h4>
+                <h4> {this.state.result}</h4>
             </form>
         );
     }
