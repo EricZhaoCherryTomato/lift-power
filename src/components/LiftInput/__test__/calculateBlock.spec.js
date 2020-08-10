1469dd5625521241b0a66671a3947d745396ef07
Eric Zhao <eric.zhao@virginaustralia.com>
2020-08-10 15:14:52 +1000

    update automation unit test with travis

--- src/components/LiftInput/__test__/calculateBlock.spec.js
+++ src/components/LiftInput/__test__/calculateBlock.spec.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import calculateBlock from '../calculateBlock'
 
 describe('calculateBlock', () => {
@@ -6,8 +5,41 @@ describe('calculateBlock', () => {
         const result = calculateBlock('');
         expect(result).toEqual('Empty command is not allowed');
     });
-    test('should handle correct command', () => {
+    test('should handle correct command with extreme big number in first row', () => {
         const result = calculateBlock('PLPLPLPLPLPLPLPLPLPL');
         expect(result).toEqual('A000000000');
+    });
+    test('should handle correct command with normal ops', () => {
+        const result = calculateBlock('PMLPMMMLPMLPMML');
+        expect(result).toEqual('0211000000');
+    });
+    test('should handle correct command with multi pickup and one drop', () => {
+        //If the robot already holds a block, Pickup will reset position to 0
+        const result = calculateBlock('PMPMPM');
+        expect(result).toEqual('0000000000');
+    });
+    test('should handle correct command with multi pickup and one drop', () => {
+        const result = calculateBlock('PPPPPPPPPPPPPPPPPPL');
+        expect(result).toEqual('1000000000');
+    });
+    test('should handle correct command with all drop', () => {
+        //Lowering without a block does nothing
+        const result = calculateBlock('LLLLLLLLLLLLLLLLLLLLL');
+        expect(result).toEqual('0000000000');
+    });
+    test('should handle correct command with extreme max number in first row', () => {
+        // Lowering the block on a pile of 15 does nothing
+        const result = calculateBlock('PLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPLPL');
+        expect(result).toEqual('F000000000');
+    });
+    test('should handle correct command by going beyound 9', () => {
+        // The robot will not go beyond position 9, Trying to move further does nothing
+        const result = calculateBlock('PMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMML');
+        expect(result).toEqual('0000000000');
+    });
+    test('should handle correct command by going just beyound 9 to 10', () => {
+        // The robot will not go beyond position 9, Trying to move further does nothing
+        const result = calculateBlock('PMMMMMMMMMML');
+        expect(result).toEqual('0000000000');
     });
 });
--- src/components/LiftInput/calculateBlock.js
+++ src/components/LiftInput/calculateBlock.js
@@ -1,29 +1,37 @@
 const calculateBlock = (command) => {
     const blockContainer = Array(10).fill(0);
-    // console.log(blockContainer);
     if (command === '') return 'Empty command is not allowed';
     var steps = command.split("");
     var index = 0;
+    var isLoaded = false;
     steps.map(step => {
-        console.log(step);
+        // console.log(step);
         switch (step) {
             case 'P':
                 index = 0;
+                isLoaded = true;
                 break;
             case 'M':
                 index++;
                 break;
             case 'L':
-                console.log(index);
-                blockContainer[index]++;
+                // console.log(index);
+                if (isLoaded && index < 10) blockContainer[index]++;
                 break;
             default:
                 console.log(index);
         }
-        console.log(index);
-        console.log(blockContainer.map((block => block.toString(16).toUpperCase())).join(''));
+        // console.log(index);
+        // console.log(blockContainer.map((block) => { if (block > 15) { return 'F' } block.toString(16).toUpperCase() }).join(''));
 
     })
-    return blockContainer.map((block => block.toString(16).toUpperCase())).join('');
+
+    const cleanBlockConatiner = blockContainer.map((block) => {
+        if (block > 15) block = 15;
+        // console.log('new block', block)
+        return block;
+    });
+    // console.log(cleanBlockConatiner);
+    return cleanBlockConatiner.map((block => block.toString(16).toUpperCase())).join('');
 }
 export default calculateBlock;
