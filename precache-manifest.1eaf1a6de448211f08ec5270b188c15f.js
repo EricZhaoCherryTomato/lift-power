ec532d758b1d3e1d36ffa5aa2f9feab85253cb0e
Eric Zhao <eric.zhao@virginaustralia.com>
2020-08-10 15:29:19 +1000

    Updates

---
+++ precache-manifest.1eaf1a6de448211f08ec5270b188c15f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfdcd49fda1eb13ff6e4ce98d72925b",
+    "url": "/lift-power/index.html"
+  },
+  {
+    "revision": "399b60c7eb91f6d74558",
+    "url": "/lift-power/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27b583f1f2e05f24e6a0",
+    "url": "/lift-power/static/js/2.dad2a3fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lift-power/static/js/2.dad2a3fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399b60c7eb91f6d74558",
+    "url": "/lift-power/static/js/main.ce465b38.chunk.js"
+  },
+  {
+    "revision": "f7b69c18a4ba5d06e684",
+    "url": "/lift-power/static/js/runtime-main.dd39a13f.js"
+  }
+]);
